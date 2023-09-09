1252da0608c367ba58e7f83d9387c71d8fd5a315
Leo Lungu <leo.lungu13@yahoo.com>
2023-09-09 08:31:24 -0400

    Added key strokes on password testing

--- app/js/keystrokes.js
+++ app/js/keystrokes.js
@@ -3,70 +3,70 @@ let keyData = [];
 let pressedKeys = {};
 
 function getCurrentTimestamp() {
-    return new Date().getTime();
+  return new Date().getTime();
 }
 
 function determineKeyType(key) {
-    if (key === "CapsLock" || key === "Shift" || key === "Backspace") {
-        return key;
-    }
-    return "Null";
+  if (key === "CapsLock" || key === "Shift" || key === "Backspace") {
+    return key;
+  }
+  return "Null";
 }
 
 function handleKeyDown(event) {
-    const key = event.key;
-    const timestamp = getCurrentTimestamp();
-    const field = event.target.getAttribute("name"); // Get the field name
+  const key = event.key;
+  const timestamp = getCurrentTimestamp();
+  const field = event.target.getAttribute("name"); // Get the field name
 
-    if (field === "password") {
-        const keyType = determineKeyType(key);
+  if (field === "passwordVerify") {
+    const keyType = determineKeyType(key);
 
-        const keyEvent = {
-            key: keyType === "Null" ? "Null" : key,
-            keyType: keyType,
-            timepressed: timestamp,
-            timereleased: null,
-        };
-        keyData.push(keyEvent);
+    const keyEvent = {
+      key: keyType === "Null" ? "Null" : key,
+      keyType: keyType,
+      timepressed: timestamp,
+      timereleased: null,
+    };
+    keyData.push(keyEvent);
 
-        pressedKeys[key] = keyEvent;
+    pressedKeys[key] = keyEvent;
 
-        console.log(`Key '${key}' (${keyType}) pressed at ${timestamp} ms`);
-    }
+    console.log(`Key '${key}' (${keyType}) pressed at ${timestamp} ms`);
+  }
 }
 
 function handleKeyUp(event) {
-    const key = event.key;
-    const timestamp = getCurrentTimestamp();
+  const key = event.key;
+  const timestamp = getCurrentTimestamp();
 
-    if (event.target.getAttribute("name") === "password") {
-        if (pressedKeys[key]) {
-            pressedKeys[key].timereleased = timestamp;
-            delete pressedKeys[key]; 
-        }
-
-        console.log(`Key '${key}' released at ${timestamp} ms`);
+  if (event.target.getAttribute("name") === "passwordVerify") {
+    if (pressedKeys[key]) {
+      pressedKeys[key].timereleased = timestamp;
+      delete pressedKeys[key];
     }
+
+    console.log(`Key '${key}' released at ${timestamp} ms`);
+  }
 }
 
-const passwordField = document.querySelector('input[name="password"]');
+const passwordField = document.querySelector('input[name="passwordVerify"]');
 passwordField.addEventListener("keydown", handleKeyDown);
 passwordField.addEventListener("keyup", handleKeyUp);
 
 function saveDataToFile() {
-    const jsonData = JSON.stringify(keyData, null, 2);
-    const blob = new Blob([jsonData], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
+  const jsonData = JSON.stringify(keyData, null, 2);
+  const blob = new Blob([jsonData], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
 
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "keypress_data.txt";
-    a.click();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "keypress_data.txt";
+  a.click();
 
-    keyData = [];
+  keyData = [];
 }
 
-document.getElementById("form").addEventListener("submit", function (e) {
-    e.preventDefault(); 
-    saveDataToFile(); 
+document.getElementById("bio-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  saveDataToFile();
 });
