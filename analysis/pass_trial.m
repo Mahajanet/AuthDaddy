c2ac144d123f084ce0da9ff1eb2b45a791c61a48
michael-khalfin <michaelk.writing@yahoo.com>
2023-09-09 23:16:12 -0400

    commented matlab code

--- analysis/pass_trial.m
+++ analysis/pass_trial.m
@@ -1,18 +1,28 @@
 function pass = pass_trial(avgs, stds, jsonStr, significance, count_threshold)
+    % we optimize the significance and count_threshold - these are our
+    % hyperparameters
 
     % jsonStr = fileread('sample.txt');
     jsonTrial = jsondecode(jsonStr);
     vals = params_for_trial;
     
+    % we keep track of the amount of successful tests, where Ha is not
+    % accepted
     successful = 0;
     for idx = 1:numel(avgs)
+        % we calculate the z score
         z_score = (vals(1, idx) - avgs(1, idx)) / stds(1, idx);
+        % then we calculate the percentile of the z score
         probability = normcdf(z_score);
+        % finally we see if the z score is statistically significant based
+        % on our threshold
         if (probability <= 1 - (significance / 2)) && (probability >= significance / 2)
             successful.increment()
         end
     end
     
+    % we ensure that the count_threshold is low-high enough for
+    % accuracy-recall
     if successful >= count_threshold
         pass = 1;
     else 
