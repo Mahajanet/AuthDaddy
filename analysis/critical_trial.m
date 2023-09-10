1f98a23ba85950057ddb5348b537057da210026d
michael-khalfin <michaelk.writing@yahoo.com>
2023-09-09 20:46:37 -0400

    i did something

--- analysis/critical_trial.m
+++ analysis/critical_trial.m
@@ -5,6 +5,10 @@ function pass = critical_trial(avgs, stds, jsonStr, prob_threshold, count_thresh
     vals = params_for_trial;
     
     successful = 0;
+
+    for idx = 1:numel(jsonTrial(:))
+        
+    end
     for idx = 1:numel(avgs)
         z_score = (vals(1, idx) - avgs(1, idx)) / stds(1, idx);
         probability = normcdf(z_score);
