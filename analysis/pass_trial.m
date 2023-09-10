5615a991c2a0a3757aad747b210e0f88e33f9227
Marko Tanevski <123792198+kanaifu@users.noreply.github.com>
2023-09-09 21:08:13 -0500

    Update pass_trial.m

--- analysis/pass_trial.m
+++ analysis/pass_trial.m
@@ -1,4 +1,4 @@
-function pass = pass_trial(avgs, stds, jsonStr, prob_threshold, count_threshold)
+function pass = pass_trial(avgs, stds, jsonStr, significance, count_threshold)
 
     % jsonStr = fileread('sample.txt');
     jsonTrial = jsondecode(jsonStr);
@@ -8,7 +8,7 @@ function pass = pass_trial(avgs, stds, jsonStr, prob_threshold, count_threshold)
     for idx = 1:numel(avgs)
         z_score = (vals(1, idx) - avgs(1, idx)) / stds(1, idx);
         probability = normcdf(z_score);
-        if probability <= prob_threshold
+        if (probability <= 1 - (significance / 2)) && (probability >= significance / 2)
             successful.increment()
         end
     end
