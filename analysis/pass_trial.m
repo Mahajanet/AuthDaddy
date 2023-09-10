ed148301ab6f871a17a73f66dc5e0ec3e7ad7a6d
Marko Tanevski <123792198+kanaifu@users.noreply.github.com>
2023-09-09 21:00:21 -0500

    Update pass_trial.m

--- analysis/pass_trial.m
+++ analysis/pass_trial.m
@@ -8,7 +8,7 @@ function pass = pass_trial(avgs, stds, jsonStr, prob_threshold, count_threshold)
     for idx = 1:numel(avgs)
         z_score = (vals(1, idx) - avgs(1, idx)) / stds(1, idx);
         probability = normcdf(z_score);
-        if probability >= prob_threshold
+        if probability <= prob_threshold
             successful.increment()
         end
     end
